2b461612cda7af71d8fec494945dc0ce4dfc539e
simei2k <137663782+simei2k@users.noreply.github.com>
2024-10-13 10:22:19 +0800

    Edited profile page and overall styles
    - changed font colour of all texts to the color we want
    - add profile pic for profile page

--- WAD2/src/components/pages/Profile.vue
+++ WAD2/src/components/pages/Profile.vue
@@ -3,5 +3,30 @@ import NavBar from '../NavBar.vue'
 </script>
 <template>
     <NavBar></NavBar>
-    <h1>this is chat</h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-3 profile-bar">
+                <img class="profile-pic" src="../../assets/service_provider_avatar.png">
+            </div>
+            <div class="col-9 profile-contents">
+                This should be other stuff
+            </div>
+        </div>
+    </div>
 </template>
+
+<style>
+.profile-bar{
+    display: block;
+    align-items: center;
+    text-align: center;
+}
+.profile-contents{
+    border: 2px white solid;
+}
+.profile-pic{
+    width: 60%;
+    height: auto;
+}
+   
+</style>
