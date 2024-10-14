364096c080fb1171bdbbf007dada304f6129c2cd
simei2k <137663782+simei2k@users.noreply.github.com>
2024-10-14 14:24:53 +0800

    revert

--- WAD2/src/components/pages/Profile.vue
+++ WAD2/src/components/pages/Profile.vue
@@ -4,19 +4,3 @@ import NavBar from '../NavBar.vue'
 <template>
     <NavBar></NavBar>
 </template>
-
-<style>
-.profile-bar{
-    display: block;
-    align-items: center;
-    text-align: center;
-}
-.profile-contents{
-    border: 2px white solid;
-}
-.profile-pic{
-    width: 60%;
-    height: auto;
-}
-   
-</style>
