e54401f461349a0925272d5f1f9026dcebc0ec92
simei2k <137663782+simei2k@users.noreply.github.com>
2024-10-16 14:07:56 +0800

    Added log in and register button to navbar and routed the respective pages

--- WAD2/src/components/pages/LogIn.vue
+++ WAD2/src/components/pages/LogIn.vue
@@ -1,8 +1,9 @@
 <script>
 import NavBar from '../NavBar.vue'
 </script>
+
 <template>
     <NavBar></NavBar>
-    <h1 style="color:white">I need to started</h1>
+    <h1 style="color:white">I need to log in</h1>
     
 </template>
