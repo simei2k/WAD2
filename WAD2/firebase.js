5aab9c2bc36343a53e4c20d90d017e3c42e49d4e
simei2k <137663782+simei2k@users.noreply.github.com>
2024-11-07 16:29:37 +0800

    Fixed registration and authentication

--- WAD2/firebase.js
+++ WAD2/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth} from "@firebase/auth"
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAXBijdCQvs-mmBfowIVZikzWhla_48vsA",
@@ -14,9 +15,15 @@ const app = initializeApp(firebaseConfig);
 export function signUp(email, password) {
     return createUserWithEmailAndPassword(auth, email, password)
 }
-export function signIn(email, password) {
-    return signInWithEmailAndPassword(auth, email, password);
-}
+export const signIn = async (email, password) => {
+  const auth = getAuth();
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error; // Rethrow the error to be caught in the component
+  }
+};
 export function signOut() {
     return FBsignOut(auth);
 }
