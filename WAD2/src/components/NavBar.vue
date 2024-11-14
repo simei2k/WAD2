b204506690da4bda532a801530774ee7697d2676
simei2k <137663782+simei2k@users.noreply.github.com>
2024-11-14 19:36:37 +0800

    Update navbar.vue

--- WAD2/src/components/NavBar.vue
+++ WAD2/src/components/NavBar.vue
@@ -24,12 +24,14 @@ export default {
         return {
             uid
         };
+        
     },
         data() {
         return {
         isMenuOpen: false,
         isMobileView: false,
         };
+    
     },
     computed: {
         isLoggedIn() {
@@ -106,10 +108,10 @@ export default {
                 <li class="nav-item">
                     <RouterLink to="/emergency" class="nav-link">Emergency</RouterLink>            
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" v-if="!uid">
                     <div class="button"><RouterLink to="/login" class="nav-link">Log In</RouterLink></div>          
                 </li>
-                <li class="nav-item">
+                <li v-if="!uid" class="nav-item">
                     <div class="button"><RouterLink to="/register" class="nav-link">Register</RouterLink></div>          
                 </li>
                 </ul>
