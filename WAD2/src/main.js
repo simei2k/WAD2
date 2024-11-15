5153da52dbeef514908af143b0e83815a28580df
cnvltdfx <saraswatins.2023@scis.smu.edu.sg>
2024-11-15 19:40:39 +0800

    Updated Chatbot.vue

--- WAD2/src/main.js
+++ WAD2/src/main.js
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js' // Import the router
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 createApp(App)
 .use(router)
+.use(Toast, {
+    transition: "Vue-Toastification__bounce",
+    maxToasts: 20,
+    newestOnTop: true
+  })
 .mount('#app')
 
 
